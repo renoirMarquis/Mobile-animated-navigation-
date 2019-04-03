72f971a027911a93195e3c786490d9fdbf118f37
Renoir Marquis <12renoir@gmail.com>
2019-04-03 19:25:21 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var burgerMenu = document.getElementById('burger-menu');
+var show =document.getElementById('menu');
+
+burgerMenu.addEventListener('click', function(){
+	this.classList.toggle("close");
+	show.classList.toggle("show");
+
+});
